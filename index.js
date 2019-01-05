6011ab0035f846f0d739d8f9e7c392a1dbb44d31
mgo4sls <46404747+mgo4sls@users.noreply.github.com>
2019-01-05 14:52:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var ctx = document.getElementById("ctx").getContext("2d");
 ctx.font = "80px Arial";
-var WIDTH = 1920;
-var HEIGHT = 1080;
+var WIDTH = 1280;
+var HEIGHT = 720;
 var background = new Image(); background.src = "blue.png";
 
 function company() {
