a8c6eb71771953a3e29d84737e30a4e11dff8a79
mgo4sls <46404747+mgo4sls@users.noreply.github.com>
2019-10-25 16:02:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var news = "HELLO";
+
+let capture;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    capture = createCapture(VIDEO);
+    capture.size(320, 240);
+	capture.hide();
+}
+
+function draw() {
+    background(255);
+    image(capture, 0, 0, windowWidth, windowHeight);
+    //filter(INVERT);
+}
+
+function keyPressed() {
+    if (keyCode === 37) {
+        news = "LEFT";
+    }
+    if (keyCode === 39) {
+        news = "RIGHT";
+    }
+    if (keyCode === 13) {
+        news = "ENTER";
+    }
+}
