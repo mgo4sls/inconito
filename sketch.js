4de0341274933b5b586f77962073a6514cd5d87a
mgo4sls <46404747+mgo4sls@users.noreply.github.com>
2019-10-25 16:28:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,35 @@
 var news = "HELLO";
 
 let capture;
+let ai;
+
+function preload() {
+  img = loadImage('a1.png');
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight*2);
+    createCanvas(windowWidth, windowHeight);
     capture = createCapture(VIDEO);
-    capture.size(320, 240);
-	capture.hide();
+    capture.size(windowWidth, windowHeight);
+    capture.hide();
+    ai = loadImage('image/a1.png');
+
+
 }
 
 function draw() {
     background(255);
     image(capture, 0, 0, windowWidth, windowHeight);
     //filter(INVERT);
+    image(iai, 0, height / 2, img.width / 2, img.height / 2);
 }
 
+
+
+
+
+
+/*
 function keyPressed() {
     if (keyCode === 37) {
         news = "LEFT";
@@ -26,3 +41,4 @@ function keyPressed() {
         news = "ENTER";
     }
 }
+*/
